187620afb3a5a6cede94f0942b316acd6a61451b
KarlaDRJ <denikerojass@gmail.com>
2025-02-23 18:13:55 -0600

    version2

--- extraccion_bd/src/app.module.ts
+++ extraccion_bd/src/app.module.ts
@@ -5,27 +5,30 @@ import { DatosService } from './datos.service';
 import { DatosController } from './datos.controller';
 import { DatosExtraidos } from './datos.entity';
 import { UsuariosModule } from './usuarios/usuarios.module'; 
-import { AuthModule } from './auth/auth.module';  // Importa AuthModule aquí
+import { AuthModule } from './auth/auth.module'; 
 import { Usuario } from './usuarios/usuario.entity';
-
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
     HttpModule,
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'Transporte',  
-      password: '1234',  
-      database: 'Bd_transporte', 
-      entities: [DatosExtraidos, Usuario],
-      synchronize: true,
+    ConfigModule.forRoot({ isGlobal: true }), // Carga el archivo .env y lo hace global
+    TypeOrmModule.forRootAsync({
+      useFactory: async (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get<string>('DATABASE_HOST'),
+        port: configService.get<number>('DATABASE_PORT'),
+        username: configService.get<string>('DATABASE_USERNAME'),
+        password: configService.get<string>('DATABASE_PASSWORD'),
+        database: configService.get<string>('DATABASE_NAME'),
+        entities: [DatosExtraidos, Usuario],
+        synchronize: true,
+      }),
+      inject: [ConfigService], // Inyecta el servicio ConfigService
     }),
     TypeOrmModule.forFeature([DatosExtraidos]),
     UsuariosModule,
     AuthModule,
- 
   ],
   controllers: [DatosController],
   providers: [DatosService],
